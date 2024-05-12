70fba8b843f72d81be9d3cbaa47f3abcb6b868b1
Luke Hunter <luke.hunter9@gmail.com>
2024-05-12 19:00:48 +1000

    Built inquirer functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,12 +3,13 @@ INSERT INTO department (name) VALUES ('Customer Service'), ('Bakery'), ('Butcher
 INSERT INTO role (title, salary, department_id) VALUES ('Cashier', 55000, 1), ('Baker', 60000, 2), ('Butcher', 65000, 3), ('Stocker', 50000, 4), ('Manager', 105000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('John', 'Doe', 1, 1),
-('Jane', 'Smith', 2, 2),
-('Mark', 'Johnson', 3, 2),
+('John', 'Doe', 1, 5),
+('Jane', 'Smith', 2, 6),
+('Mark', 'Johnson', 3, 6),
 ('Emily', 'Williams', 4, 1),
-('Chris', 'Brown', 5, 3),
-('Emma', 'Davis', 5, 3),
-('Michael', 'Wilson', 1, 1),
-('Olivia', 'Martinez', 4, 1);
+('Chris', 'Brown', 5, 7),
+('Emma', 'Davis', 5, 7),
+('Wayne', 'Stevenson', 5, NULL),
+('Michael', 'Wilson', 1, 5),
+('Olivia', 'Martinez', 4, 5);
 
---
+++ index.js
@@ -0,0 +1,74 @@
+var inquirer = require('inquirer');
+
+const runInq = async () => {
+    let answer
+    try {
+        answer = await inquirer.prompt([
+            /* Pass your questions in here */
+            {
+                type: 'list', name: 'menu', choices: ['view all departments',
+                    'view all roles',
+                    'view all employees',
+                    'add a department',
+                    'add a role',
+                    'add an employee',
+                    'update an employee role'
+                ]
+            }
+        ])
+
+
+    } catch (error) {
+        if (error.isTtyError) {
+            console.error("Prompt couldn't be rendered in the current environment");
+        } else {
+            console.error(error);
+        }
+    }
+
+    const question = []
+    switch (answer.menu) {
+        case 'view all roles':
+
+        case 'view all employees':
+
+        case 'add a department':
+            question.push({ type: 'input', name: 'department', message: 'What is the department name? ' });
+            break
+
+        case 'add a role':
+            question.push(
+                { type: 'input', name: 'role', message: 'What is the role? ' },
+                { type: 'input', name: 'salary', message: 'What is the salary ' }
+            );
+            break
+
+        case 'add an employee':
+            question.push(
+                { type: 'input', name: 'firstName', message: 'What is the first name? ' },
+                { type: 'input', name: 'lastName', message: 'What is the last name? ' },
+                { type: 'input', name: 'role', message: 'What is the role? ' },
+                // Will be updated to list of managers
+                { type: 'input', name: 'role', message: 'Who is the employees manager? ' }
+            );
+            break
+
+        case 'update an employee role':
+            question.push(
+                // Will be updated to list of employees
+                { type: 'input', name: 'employee', message: 'Which employees role would you like to update? ' },
+                // Will be updated to list of roles
+                { type: 'input', name: 'roles', message: 'Which role do you want to assign? ' }
+            );
+            break
+
+        default:
+            console.log('Quitting Program');
+    }
+    answer = await inquirer.prompt(question);
+    console.log(answer);
+};
+
+// Will need to utilise recursion to get the function to call itself.
+runInq()
+
